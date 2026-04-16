0fa5266461b101d440a107143b9cc5e3bf901743
Adi050607 <adityabennuru69@gmail.com>
2026-04-16 21:26:55 +0530

    serve frontend from backend

--- server.js
+++ server.js
@@ -56,3 +56,9 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log("Server running on port " + PORT);
 });
+const path = require("path");
+
+app.use(express.static(path.join(__dirname)));
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "index.html"));
+});
